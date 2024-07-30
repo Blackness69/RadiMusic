1ca6b343812b3fc31448fc135cfa2836e023c036
Blackness69 <161008491+Blackness69@users.noreply.github.com>
2024-07-30 16:09:09 +0000

    .

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 //config.js
+const { readFileSync } = require('fs');
+
 module.exports = {
   prefix: ".", // your bot prefix
-  token: process.env.token,
-  clientId: process.env.CLIENT_ID, // add CLIENT_ID in .env / for replit user add CLIENT_ID in Secrets
+  token: process.env.token || readFileSync('token.txt', 'utf-8'),
+  clientId: process.env.CLIENT_I|| readFileSync('CLIENT_ID.txt', 'utf-8'), // add CLIENT_ID in .env / for replit user add CLIENT_ID in Secrets
   ownerId: "", // add owner ID (optional) 
   mongoURL: process.env.mongoURL, // add your mongoURL in .env / for replit users add your mongoURL in Secrets
 };
